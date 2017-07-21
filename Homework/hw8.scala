1e6956667a1f3a6517773952d35f76163258d04a
Netjimmy <jimmy@172-16-195-241.DYNAPOOL.NYU.EDU>
2017-07-20 23:43:07 -0400

    add homework8

---
+++ Homework/hw8.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+val sqlCtx = new SQLContext(sc)
+import sqlCtx._
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+
+val articleDF = sqlCtx.jsonFile("/user/cyy292/project/wsjArticle/*")
+
+val tokenizer = new Tokenizer().setInputCol("content").setOutputCol("words")
+val wordsData = tokenizer.transform(articleDF)
+val hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(100)
+val featurizedData = hashingTF.transform(wordsData)
+val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+val idfModel = idf.fit(featurizedData)
+val rescaledData = idfModel.transform(featurizedData)
+// rescaledData.select("features", "label").take(3).foreach(println)
+
+val genData = rescaleData.rdd
