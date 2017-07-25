4c0109d732a049751f9822d801ec7847b103f92a
Netjimmy <jimmy@Netjimmys-MBP.fios-router.home>
2017-07-25 02:34:58 -0400

    modified TFIDF

--- Models/TFIDF.scala
+++ Models/TFIDF.scala
@@ -1,14 +1,11 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 val sqlCtx = new SQLContext(sc)
-import sqlCtx._
 import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
 
-# Simple TF-IDF Model for Sentiment Analysis
+//Simple TF-IDF Model for Sentiment Analysis
 
-val articleDF = sqlCtx.jsonFile("/user/cyy292/project/wsjArticle/*")
-
+val articleDF = sqlCtx.read.json("/Users/jimmy/Desktop/tmp/2017-07-01.json")
+//
 val tokenizer = new Tokenizer().setInputCol("content").setOutputCol("words")
 val wordsData = tokenizer.transform(articleDF)
 val hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(100)
@@ -16,6 +13,7 @@ val featurizedData = hashingTF.transform(wordsData)
 val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
 val idfModel = idf.fit(featurizedData)
 val rescaledData = idfModel.transform(featurizedData)
-// rescaledData.select("features", "label").take(3).foreach(println)
-
-val genData = rescaleData.rdd
+// rescaledData.select("features").take(3).foreach(println)
+val featureVec = udf{x:SparseVector=>x.toArray}
+val rescaledData_get = rescaledData.withColumn("features_trans", featureVec(rescaledData("features")))
+rescaledData_get.select("features", "features_trans").take(3).foreach(println)
